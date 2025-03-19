fd111026db583a8531640aa7c4fcb15f5197d64f
Ilya Gelvikh <pochtabang@gmail.com>
2025-03-19 09:06:22 +0700

    Initial commit

---
+++ src/main/java/ru/ilya/code/ApplicationJDBC/Main.java
@@ -0,0 +1,29 @@
+package ru.ilya.code.ApplicationJDBC;
+
+import java.sql.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main {
+	public static final String User = "User";
+	public static final String url = "jdbc:mysql://localhost:3306/workers";
+	public static final String Password = "246810";
+	
+    public static void main( String[] args ) {
+    	try(Connection conection =  DriverManager.getConnection(url, User, Password);) {
+    		
+    		String sql = "CREATE TABLE work (" +
+    				"id INT AUTO_INCREMENT PRIMARY KEY, " +
+                    "name VARCHAR(100) NOT NULL )";
+    	
+    		Statement statement = conection.createStatement();
+    		statement.executeUpdate(sql);
+    		
+    		System.out.println("Connection is open");
+    	} catch(SQLException ex) {
+    		System.out.print("Sql get wrong" + ex.getMessage());
+    	} 
+    }
+}
